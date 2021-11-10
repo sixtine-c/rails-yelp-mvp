28ee6736f86f1e548b63cd0db8e13d577c104680
Sixtine Caspar <sixcaspar@gmail.com>
2021-11-10 15:06:41 +0100

    done with no front

---
+++ app/models/review.rb
@@ -0,0 +1,6 @@
+class Review < ApplicationRecord
+  RATINGS = [0, 1, 2, 3, 4, 5]
+  belongs_to :restaurant
+  validates :content, :rating, presence: true
+  validates :rating, inclusion: { in: RATINGS }, numericality: {only_interger: true}
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'cleaning the table'
+Restaurant.destroy_all
+
+puts 'adding 5 new restaurants'
+Restaurant_1 = Restaurant.create(name: "Epicure", category: "french", address: 'Paris')
+Restaurant_2 = Restaurant.create(name: "Mamma Primmi", category: "italian", address: 'Paris')
+Restaurant_3 = Restaurant.create(name: "Sushi Shop", category: "japanese", address: 'Paris')
+Restaurant_4 = Restaurant.create(name: "La cantine", category: "french", address: 'Lyon')
+Restaurant_5 = Restaurant.create(name: "Les moules frites", category: "belgian", address: 'Lyon')
+Restaurant_6 = Restaurant.create(name: "pekin express", category: "chinese", address: 'Nantes')
+puts 'done'
